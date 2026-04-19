1062cf73d3008f66c39b69a76c4227b05fc55a0c
Thomas Baumhauer <thomas@thomba.de>
2026-04-19 07:50:49 +0200

    Update Gradle, Kotlin, and Android Gradle Plugin versions
    - Updated Android Gradle Plugin from 8.13.2 to 9.1.1.
    - Upgraded Kotlin and Compose plugin versions from 2.1.10 to 2.2.10.
    - Updated Gradle wrapper to version 9.3.1.
    - Added various Android build feature flags and compatibility properties to `gradle.properties`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
     id("org.jetbrains.kotlin.jvm") version "2.1.10" apply false
 }
 
